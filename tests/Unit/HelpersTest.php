917ab238a342f74137cb3405639bbb626882b75b
Ondrej Vrťo <32365016+OndrejVrto@users.noreply.github.com>
2023-01-11 11:41:34 +0100

    simplify test helpers

--- tests/Unit/HelpersTest.php
+++ tests/Unit/HelpersTest.php
@@ -5,19 +5,13 @@ use OndrejVrto\Visitors\Traffic;
 use OndrejVrto\Visitors\Statistics;
 
 test('helper visit return visit object', function () {
-    $visit = visit();
-
-    expect($visit)->toBeObject()->and($visit)->toBeInstanceOf(Visit::class);
+    expect(visit())->toBeInstanceOf(Visit::class);
 });
 
 test('helper traffic return traffic object', function () {
-    $traffic = traffic();
-
-    expect($traffic)->toBeObject()->and($traffic)->toBeInstanceOf(Traffic::class);
+    expect(traffic())->toBeInstanceOf(Traffic::class);
 });
 
 test('helper statistics return statistics object', function () {
-    $statistics = statistics();
-
-    expect($statistics)->toBeObject()->and($statistics)->toBeInstanceOf(Statistics::class);
+    expect(statistics())->toBeInstanceOf(Statistics::class);
 });
