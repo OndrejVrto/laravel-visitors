a7cf42fedb0b192ec10098665a498f82e8d08b8a
Ondrej Vrťo <32365016+OndrejVrto@users.noreply.github.com>
2023-01-11 11:41:08 +0100

    firs test with db

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace OndrejVrto\Visitors\Tests;
 
+use PDO;
 use Closure;
 use Mockery;
 use Carbon\Carbon;
@@ -14,8 +15,10 @@ use OndrejVrto\Visitors\VisitorsServiceProvider;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
+use function Orchestra\Testbench\artisan;
+
 class TestCase extends Orchestra {
-    // use RefreshDatabase;
+    use RefreshDatabase;
 
     /**
      * Setup the test environment.
@@ -28,12 +31,6 @@ class TestCase extends Orchestra {
         Factory::guessFactoryNamesUsing(
             fn (string $modelName) => 'OndrejVrto\\Visitors\\Database\\Factories\\'.class_basename($modelName).'Factory'
         );
-
-        // $this->destroyPackageMigrations();
-        // $this->publishPackageMigrations();
-        // $this->migratePackageTables();
-        // $this->migrateUnitTestTables();
-        // $this->registerPackageFactories();
     }
 
     /**
@@ -68,35 +65,89 @@ class TestCase extends Orchestra {
      * @param  \Illuminate\Foundation\Application  $app
      */
     public function getEnvironmentSetUp($app): void {
-        // Setup default database to use sqlite :memory:
-        $app['config']->set('database.default', 'sqlite');
-        $app['config']->set('database.connections.sqlite', [
-            'driver' => 'sqlite',
-            'database' => ':memory:',
+        $app['config']->set('database.connections.mysql', [
+            'driver' => 'mysql',
+            'url' => env('DATABASE_URL'),
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => 'test_visitors_laravel',
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            // 'collation' => 'utf8mb4_unicode_ci',
+            'collation' => 'utf8mb4_slovak_ci',
             'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => true,
+            'engine' => null,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
         ]);
 
-        $app['db']->connection()->getSchemaBuilder()->create('test_models', function (Blueprint $table) {
-            $table->increments('id');
-            $table->timestamps();
-        });
+        $app['config']->set('database.connections.mysql_visitors', [
+            'driver' => 'mysql',
+            'url' => env('DATABASE_URL'),
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => 'test_visitors_package',
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            // 'collation' => 'utf8mb4_unicode_ci',
+            'collation' => 'utf8mb4_slovak_ci',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => true,
+            'engine' => null,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
+        ]);
+        $app['config']->set('database.default', 'mysql');
+        $app['config']->set('visitors.eloquent_connection', 'mysql_visitors');
 
-        // TODO: migrate visitors table to database connection from config file
-        // $migration = include __DIR__.'/../database/migrations/create_all_visitors_tables.php.stub';
-        // $migration->up();
+        // dd($app['config']);
     }
 
+    /**
+     * Define database migrations.
+     */
+    protected function defineDatabaseMigrations(): void {
+        $this->loadLaravelMigrations();
+        $this->loadMigrationsFrom(__DIR__ . '/Support/migrations');
+    }
 
+    /**
+     * Perform any work that should take place before the database has started refreshing.
+     */
+    protected function beforeRefreshingDatabase(): void {
+        $migration = include __DIR__.'/../database/migrations/create_all_visitors_tables.php.stub';
+        $migration->down();
+        // $this->publishPackageMigrations();
+    }
 
-
+    /**
+     * Perform any work that should take place once the database has finished refreshing.
+     */
+    protected function afterRefreshingDatabase(): void {
+        // $this->destroyPackageMigrations();
+        $migration = include __DIR__.'/../database/migrations/create_all_visitors_tables.php.stub';
+        $migration->up();
+    }
 
     /**
      * Clean up the testing environment before the next test.
      */
-    // protected function tearDown(): void {
-    //     Mockery::close();
-    //     Carbon::setTestNow();
-    // }
+    protected function tearDown(): void {
+        $migration = include __DIR__.'/../database/migrations/create_all_visitors_tables.php.stub';
+        $migration->down();
+
+        // Mockery::close();
+        // Carbon::setTestNow();
+    }
 
     /**
      * Publish package migrations.
@@ -104,7 +155,7 @@ class TestCase extends Orchestra {
     // protected function publishPackageMigrations(): void {
     //     $this->artisan('vendor:publish', [
     //         '--force' => '',
-    //         '--tag' => 'migrations',
+    //         '--tag' => 'visitors-migrations',
     //     ]);
     // }
 
@@ -112,7 +163,7 @@ class TestCase extends Orchestra {
      * Delete all published migrations.
      */
     // protected function destroyPackageMigrations(): void {
-    //     File::cleanDirectory('vendor/orchestra/testbench-core/laravel/database/migrations');
+    //     File::cleanDirectory('./vendor/orchestra/testbench-core/laravel/database/migrations');
     // }
 
     /**
---
+++ tests/Unit/ModelTests/VisitorDataTest.php
@@ -0,0 +1,16 @@
+<?php
+
+use OndrejVrto\Visitors\Tests\TestCase;
+use OndrejVrto\Visitors\Models\VisitorsData;
+
+uses(TestCase::class);
+
+it('can have a custom connection through config file', function () {
+    config()->set('visitors.eloquent_connection', 'mysql');
+
+    expect('mysql')->toBe((new VisitorsData())->getConnectionName());
+
+    config()->set('visitors.eloquent_connection', 'mysql_visitors');
+
+    expect('mysql_visitors')->toBe((new VisitorsData())->getConnectionName());
+})->only();
