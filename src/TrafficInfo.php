5d2cff76dabb9bc545453f56cb32e0151c923d56
Ondrej Vrťo <32365016+OndrejVrto@users.noreply.github.com>
2023-01-28 23:04:27 +0100

    retype attribute after casting

--- src/TrafficInfo.php
+++ src/TrafficInfo.php
@@ -4,12 +4,13 @@ declare(strict_types=1);
 
 namespace OndrejVrto\Visitors;
 
+use Carbon\Carbon;
 use OndrejVrto\Visitors\Models\VisitorsData;
 use OndrejVrto\Visitors\Models\VisitorsInfo;
 use OndrejVrto\Visitors\Data\VisitorsInfoData;
 
 class TrafficInfo {
-    public static function get(): VisitorsInfoData {
+    public static function get(): ?VisitorsInfoData {
         $info = VisitorsInfo::query()
             ->latest()
             ->first();
@@ -18,15 +19,34 @@ class TrafficInfo {
             ->orderByDesc('visited_at')
             ->first();
 
+        if (null === $info || null === $lastData ) {
+            return null;
+        }
+
+        /** @var integer */
+        $count_rows = $info->getAttribute('count_rows');
+        /** @var Carbon */
+        $from = $info->getAttribute('from');
+        /** @var Carbon */
+        $to = $info->getAttribute('to');
+        /** @var integer */
+        $last_data_id = $info->getAttribute('last_data_id');
+        /** @var Carbon */
+        $created_at = $info->getAttribute('created_at');
+        /** @var integer */
+        $recent_visit_count = $lastData->getAttribute('data_id') - $info->getAttribute('last_data_id');
+        /** @var Carbon */
+        $recent_visit_date = $lastData->getAttribute('visited_at');
+
         //TODO: rename properties in DTO
         return new VisitorsInfoData(
-            count_rows:         $info->getAttribute('count_rows'),
-            from:               $info->getAttribute('from'),
-            to:                 $info->getAttribute('to'),
-            last_data_id:       $info->getAttribute('last_data_id'),
-            created_at:         $info->getAttribute('created_at'),
-            recent_visit_count: $lastData->getAttribute('data_id') - $info->getAttribute('last_data_id'),
-            recent_visit_date:  $lastData->getAttribute('visited_at'),
+            to:                 $to,
+            from:               $from,
+            created_at:         $created_at,
+            count_rows:         $count_rows,
+            last_data_id:       $last_data_id,
+            recent_visit_date:  $recent_visit_date,
+            recent_visit_count: $recent_visit_count,
         );
     }
 }
